b5228183a32761c64af05ee6efd4345f04320a9a
박요진 <syon013@naver.com>
2023-12-20 21:29:17 +0900

    🚀 version 1.1.1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],  theme: {
+    extend: {},
+    colors: {
+      primary: "#eb2f06",
+      secondary: "#f39c12",
+      tertiary: "#ffc312",
+    }
+  },
+  plugins: [],
+}
+
